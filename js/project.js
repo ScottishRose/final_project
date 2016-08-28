400a63ddb6a99cd2a1d96152504f58b8caef8be0
Stef Boone <stefboone@gmail.com>
2016-08-28 14:05:43 -0400

    working changes

--- js/project.js
+++ js/project.js
@@ -1,4 +1,11 @@
-$('.masonry').masonry({
-  itemSelector: '.masonry-item',
-  columnWidth: 200
-});
+// $('.masonry').masonry({
+//   itemSelector: '.masonry-item',
+//   columnWidth: 200
+// });
+$(function(){
+      $('.element').typed({
+        strings: ['Creator.', 'Photographer.', 'Maker.', 'Crafter.', 'Filmmaker.', 'Coder.'],
+        typeSpeed: 5000
+      });
+  });
+
